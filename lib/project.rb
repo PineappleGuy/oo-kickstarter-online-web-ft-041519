ff677abe50806e46396fad666bd330f9f3cc1929
Garth Lilly <garff247@gmail.com>
2019-05-10 05:06:44 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -2,10 +2,10 @@ require 'pry'
 class Project
 
   attr_accessor :backers
-  attr_reader :name
+  attr_reader :title
 
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @backers = []
   end
 
